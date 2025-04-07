bf88cbd965a6f9bb6872354b6fae069b8ff3954f
wima-404 <blkwiam2003@gmail.com>
2025-04-07 16:29:06 +0200

    Architecture angular and Routage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ContactComponent } from './components/contact/contact.component';
+export const routes: Routes = [
+    {path:'home', component:HomeComponent, data:{ animation:'HomePage'}},
+    {path:'about' , component:AboutComponent, data:{ animation:'AboutPage'}},
+    {path:'projects', component:ProjectsComponent, data:{animation:'ProjectPage'}},
+    {path:'contact', component:ContactComponent, data:{animation:'ContactPage'}},
+    {path:'', redirectTo:'/home', pathMatch:'full'},
+    {path:'**',redirectTo:'/home'}
+];
